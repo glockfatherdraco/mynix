492dd135701c1d3c4bf158d58b6bf1f78940d193
glockfatherdraco <161503241+glockfatherdraco@users.noreply.github.com>
2026-09-26 14:52:09 +0200

    fix extension install patterns in librewolf

--- home/data/librewolf/extensions.nix
+++ home/data/librewolf/extensions.nix
@@ -1,5 +1,5 @@
 {
-  # Bihwarden
+  # Bitwarden
   "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
     installation_mode = "force_installed";
@@ -7,20 +7,22 @@
 
   # Canvas Blocker
   "CanvasBlocker@kkapsner.de" = {
-    install_url = "https://addons.mozilla.org/firefox/downloads/file/4262820/canvasblocker-latest.xpi";
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/canvasblocker/latest.xpi";
     installation_mode = "force_installed";
     default_area = "menupanel";
   };
 
   # Get RSS Feed URL
-  "{fdf5fb9f-e608-4100-8cb8-726715f21051}" = {
+  "{15bdb1ce-fa9d-4a00-b859-66c214263ac0}" = {
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/get-rss-feed-url/latest.xpi";
     installation_mode = "force_installed";
+    default_area = "navbar";
   };
 
-  # Nix PR Tracker
-  "{a88f7a83-a9d2-43bb-81d3-3c94294d13e5}" = {
-    install_url = "https://addons.mozilla.org/firefox/downloads/latest/nix-pr-tracker/latest.xpi";
+  # Nixpkgs PR Tracker
+  "nixpkgs-pr-tracker@tahayassine.me" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/nixpkgs-pr-tracker/latest.xpi";
     installation_mode = "force_installed";
+    default_area = "menupanel";
   };
 }
